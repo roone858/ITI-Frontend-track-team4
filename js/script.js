dab4269e93d1a3ea81d6d9902b47c94a47a66519
rooe858 <roone858@gmail.com>
2022-09-21 16:29:36 +0200

    the finaly

---
+++ js/script.js
@@ -0,0 +1,91 @@
+let firstname = document.getElementById("firstname");
+let lastname = document.getElementById("lastname");
+let email = document.getElementById("email");
+let reviewTitle = document.getElementById("review-title");
+let rate = document.getElementById("rate");
+let textarea = document.getElementById("textarea");
+let submit = document.getElementById("submit");
+var today = new Date();
+
+let reviewsArr = [];
+submit.addEventListener("click", function () {
+  if (isInputsEmpty()) {
+    reviewsArr.push({
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      reviewTitle: reviewTitle.value,
+      rate: rate.value,
+      textarea: textarea.value,
+    });
+
+    clearTheValues();
+    addToResultContainer();
+  } else {
+    alert("you must enter all inputs");
+  }
+});
+function addToResultContainer() {
+  document.querySelector(".result-container").innerHTML += translateObjToHTML(
+    reviewsArr[reviewsArr.length - 1]
+  );
+}
+function translateObjToHTML(object) {
+  return ` 
+    <div class="rev">
+    <h2>${object.firstname} ${object.lastname}</h2>
+      <p class="rate-date">rating :<span class="Date"> ${
+        object.rate
+      } / 5  </span>   &emsp; &emsp; &emsp;   ${today.getDate()} / ${
+    +today.getMonth() + 1
+  } / ${today.getFullYear()}</p>
+      <p class="text-optional">
+        ${object.textarea}
+      </p>
+
+    </div>`;
+}
+function clearTheValues() {
+  for (const input of document.querySelectorAll("input,textarea")) {
+    input.value = "";
+  }
+}
+function isInputsEmpty() {
+  for (const input of document.querySelectorAll(".reviews input")) {
+    if (input.value == "") {
+      return false;
+    }
+  }
+  return true;
+}
+rate.addEventListener("click", function () {
+  document.querySelector("label").innerText = `${rate.value} / 5`;
+});
+let pic = document.querySelector('.home')
+
+let pics = [
+  "/imgs/2945044.jpg",
+  "/imgs/1657871.jpg",
+  "/imgs/40-of-the-Coolest-Web-Designs-with-Pattern-Backgrounds.jpg",
+  "/imgs/web-design-concept-presentation-banner-ad-advertising-29993024.jpg",
+];
+let i = 1;
+
+function updateHomePic() {
+  let photo = pics[i];
+
+ 
+  pic.innerHTML = ` <img 
+  style="width:100%;height:100%" 
+  src=${photo} 
+  alt="" />`;
+ 
+
+  i++;
+
+  if (i > pics.length - 1) {
+    i = 0;
+  }
+}
+
+setInterval(updateHomePic, 4000);
---
+++ js/scripttestmonial.js
@@ -0,0 +1,45 @@
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const testimonial = document.querySelector(".testimonial");
+const userImage = document.querySelector(".user-image");
+const username = document.querySelector(".username");
+const role = document.querySelector(".role");
+
+const testimonials = [
+  {
+    name: "Mahmoud Gamal",
+    position: "Dev Ops",
+    photo: "https://randomuser.me/api/portraits/men/97.jpg",
+    text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+  },
+  {
+    name: "Abdullah Mohamed",
+    position: "Software Engineer",
+    photo: "https://randomuser.me/api/portraits/men/97.jpg",
+    text: "This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!",
+  },
+  {
+    name: "Sherin Raffat",
+    position: "Data Entry",
+    photo: "https://randomuser.me/api/portraits/men/97.jpg",
+    text: "She is a hard worker. Communication was also very good with her and She was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+  },
+];
+
+let idx = 1;
+
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
+
+  idx++;
+
+  if (idx > testimonials.length - 1) {
+    idx = 0;
+  }
+}
+
+setInterval(updateTestimonial, 4000);
